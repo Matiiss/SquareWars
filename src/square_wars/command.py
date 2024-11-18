18131b6045d5f3706a536fec4edee557ae594035
JiffyRob <jfrobinson767@gmail.com>
2024-11-17 22:59:56 -0600

    coded up BFS based AI

--- src/square_wars/command.py
+++ src/square_wars/command.py
@@ -1,4 +1,5 @@
 import queue
+import collections
 import pygame
 
 from . import common
@@ -18,14 +19,30 @@ class Command:
     def __init__(self, command_name):
         self.command_name = command_name
 
-class InputController:
+
+class Controller:
+    def __init__(self):
+        self.sprite = None
+        self.command_queue = queue.Queue()
+
+    def register_sprite(self, sprite):
+        self.sprite = sprite
+
+    def update(self):
+        pass
+
+
+class InputController(Controller):
     def __init__(self, up_key=pygame.K_w, down_key=pygame.K_s, left_key=pygame.K_a, right_key=pygame.K_d, shoot_key=pygame.K_RSHIFT):
+        super().__init__()
         self.up_key = up_key
         self.down_key = down_key
         self.left_key = left_key
         self.right_key = right_key
         self.shoot_key = shoot_key
-        self.command_queue = queue.Queue()
+
+    def register_sprite(self, sprite):
+        self.sprite = sprite
 
     def update(self):
         for event in common.events:
@@ -49,3 +66,78 @@ class InputController:
                     self.command_queue.put(Command(COMMAND_STOP_LEFT))
                 if event.key == self.right_key:
                     self.command_queue.put(Command(COMMAND_STOP_RIGHT))
+
+
+class DumbAIController(Controller):
+    def __init__(self):
+        super().__init__()
+        self.team = None
+        self.pathfind_queue = queue.Queue()
+        self.initial_frame = True
+
+    def register_sprite(self, sprite):
+        super().register_sprite(sprite)
+        self.team = sprite.team
+
+    def pathfind(self):
+        # find nearest target square using BFS
+        x, y = int(self.sprite.rect.x / 8), int(self.sprite.rect.y / 8)
+        frontier = queue.Queue()
+        frontier.put((x, y))
+        came_from = {(x, y): None}
+        grid = common.current_state.squares
+        target_position = None
+
+        while target_position is None:
+            if frontier.empty():
+                break
+            current = frontier.get()
+            for next in grid.get_neighbors(current):
+                if next not in came_from:
+                    frontier.put(next)
+                    came_from[next] = current
+                    if grid.get_sprite_by_coordinate(*next).team != self.team:
+                        target_position = next
+        if target_position is None:
+            return False
+        # contruct path of coordinates to that square
+        path = []
+        current = target_position
+        while current != (x, y): 
+            path.append(current)
+            current = came_from[current]
+        path.reverse()
+        # convert path of coordinates to commands
+        current = (x, y)
+        directions = {
+            (0, -1): COMMAND_UP,
+            (0, 1): COMMAND_DOWN,
+            (-1, 0): COMMAND_LEFT,
+            (1, 0): COMMAND_RIGHT
+        }
+        stops = {
+            COMMAND_UP: COMMAND_STOP_UP,
+            COMMAND_DOWN: COMMAND_STOP_DOWN,
+            COMMAND_LEFT: COMMAND_STOP_LEFT,
+            COMMAND_RIGHT: COMMAND_STOP_RIGHT,
+        }
+        print((x, y), path)
+        for coord in path:
+            direction = directions[coord[0] - current[0], coord[1] - current[1]]
+            self.pathfind_queue.put(direction)
+            self.pathfind_queue.put(stops[direction])
+            current = coord
+        return True
+
+    def update(self):
+        if self.initial_frame:
+            self.pathfind()
+            self.command_queue.put(Command(self.pathfind_queue.get()))
+            self.initial_frame = False
+        if self.sprite.half_aligned and self.pathfind_queue.qsize():
+            self.command_queue.put(Command(self.pathfind_queue.get()))
+            if self.pathfind_queue.qsize():
+                self.command_queue.put(Command(self.pathfind_queue.get()))
+        if self.pathfind_queue.empty() and self.sprite.aligned:
+            if self.pathfind():
+                self.command_queue.put(Command(self.pathfind_queue.get()))
--- src/square_wars/states/gameplay.py
+++ src/square_wars/states/gameplay.py
@@ -21,8 +21,21 @@ class Player(pygame.sprite.Sprite):
         self.moving = [0, 0]
         self.facing = [0, 0]
         self.command_queue = queue.Queue()
+        self.squares = pygame.sprite.Group()
 
-    def update(self):
+        self.controller.register_sprite(self)
+
+    @property
+    def aligned(self):
+        return int(self.rect.x) % 8 == 0 and int(self.rect.y) % 8 == 0
+
+    @property
+    def half_aligned(self):
+        return (int(self.rect.x) % 8, int(self.rect.y) % 8) in {(0, 4), (4, 0), (4, 4)}
+
+    def update(self) -> None:
+        # Do command reading in 2 stages
+        # Stage 1: realtime, cache non-realtime commands
         self.controller.update()
         while self.controller.command_queue.qsize():
             next_command = self.controller.command_queue.get()
@@ -30,9 +43,9 @@ class Player(pygame.sprite.Sprite):
                 print("pew pew")
             else:
                 self.command_queue.put(next_command)
-        if int(self.rect.x) % 8 == 0 and int(self.rect.y) % 8 == 0:
-            # self.rect.x = int(self.rect.x)
-            # self.rect.y = int(self.rect.y)
+        # Stage 2: evaluate motion commands only when player is aligned with the grid
+        # Ensures that the player cannot stop motion or change direction when not aligned
+        if self.aligned:
             while self.command_queue.qsize():
                 next_command = self.command_queue.get()
                 match next_command:
@@ -60,9 +73,7 @@ class Player(pygame.sprite.Sprite):
                         self.facing[0] = 1
                     case command.Command(command_name=command.COMMAND_STOP_RIGHT):
                         self.moving[0] -= 1
-
-        print(self.moving)
-                
+        # actual motion                
         if pygame.Vector2(self.moving):
             self.velocity = pygame.Vector2(self.moving)
             self.velocity.scale_to_length(self.SPEED)
@@ -73,24 +84,41 @@ class Player(pygame.sprite.Sprite):
 
 
 class Square(pygame.sprite.Sprite):
-    def __init__(self, pos, player_group):
+    def __init__(self, pos, player_group, blank_group, orange_group, brown_group):
         super().__init__()
         self.rect = pygame.FRect(0, 0, 8, 8)
         self.rect.topleft = pos
         self.player_group = player_group
+        self.team_groups = {
+            settings.TEAM_NONE: blank_group,
+            settings.TEAM_ORANGE: orange_group,
+            settings.TEAM_BROWN: brown_group
+        }
         self.team = settings.TEAM_NONE
+        self.team_group = self.team_groups[self.team]
+        self.team_group.add(self)
         self.owner = None
         self.image = pygame.Surface((8, 8)).convert()
         self.image.fill(settings.BLANK_COLOR)
+        self._x = 0
+        self._y = 0
 
     def update(self):
+        # check if I collide with any players and change color to match
         changed = False
         for sprite in pygame.sprite.spritecollide(self, self.player_group, False, center_point_collide):
             if sprite is not self.owner:
                 self.team = sprite.team
                 self.owner = sprite
+                self.owner.squares.add(self)
                 changed = True
         if changed:
+            # update team groups to reflect new ownership
+            self.team_group.remove(self)
+            self.team_group = self.team_groups[self.team]
+            self.team_group.add(self)
+            self.owner.squares.remove(self)
+            # change color
             if self.team == settings.TEAM_BROWN:
                 color = settings.BROWN_COLOR
             if self.team == settings.TEAM_ORANGE:
@@ -100,15 +128,45 @@ class Square(pygame.sprite.Sprite):
             self.image.fill(color)
 
 
+class SquareSpriteGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.grid = {}
+
+    def add_to_grid(self, sprite, x, y):
+        self.grid[(x, y)] = sprite
+        sprite._x = x
+        sprite._y = y
+        self.add(sprite)
+
+    def get_neighbors(self, sprite):
+        if isinstance(sprite, tuple):
+            x, y = sprite
+        else:
+            x, y = sprite._x, sprite._y
+        for nx in range(x - 1, x + 2):
+            for ny in range(y - 1, y + 2):
+                if abs(nx - x) + abs(ny - y) == 1 and (nx, ny) in self.grid.keys():
+                    yield nx, ny
+
+    def get_sprite_by_coordinate(self, x, y):
+        return self.grid[(x, y)]
+
 
 class Gameplay:
     def __init__(self):
         self.sprites = pygame.sprite.Group()
         self.players = pygame.sprite.Group()
-        for x in range(0, 64, 8):
-            for y in range(0, 64, 8):
-                self.sprites.add(Square((x, y), self.players))
-        controller = command.InputController()
+        self.squares = SquareSpriteGroup()
+        self.blanks = pygame.sprite.Group()
+        self.oranges = pygame.sprite.Group()
+        self.browns = pygame.sprite.Group()
+        for x in range(0, 8):
+            for y in range(0, 8):
+                sprite = Square((x * 8, y * 8), self.players, self.blanks, self.oranges, self.browns)
+                self.sprites.add(sprite)
+                self.squares.add_to_grid(sprite, x, y)
+        controller = command.DumbAIController()
         player = Player(controller, (0, 0), settings.TEAM_ORANGE)
         self.sprites.add(player)
         self.players.add(player)
