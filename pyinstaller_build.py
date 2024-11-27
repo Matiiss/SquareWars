d8802e95328d2f8bd6d3613c2be862d52009c4bf
JiffyRob <jfrobinson767@gmail.com>
2024-11-26 19:42:40 -0600

    complete build system

--- pyinstaller_build.py
+++ pyinstaller_build.py
@@ -2,6 +2,7 @@ import PyInstaller.__main__ as pyi
 import os
 import shutil
 import platform
+import pathlib
 
 # get ready for some super hax code
 pyi.run([
@@ -18,3 +19,8 @@ shutil.rmtree(export_direction, ignore_errors=True)
 shutil.copytree("dist/SquareWars", export_direction)
 shutil.copytree("src/res", f"{export_direction}/src/res", dirs_exist_ok=True)
 shutil.make_archive(f"SquareWars-{platform.system()}", "zip", export_direction)
+
+shutil.rmtree("build")
+shutil.rmtree("dist")
+shutil.rmtree(export_direction)
+pathlib.Path("./SquareWars.spec").unlink()
